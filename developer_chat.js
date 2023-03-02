882ee6684cb605f440e2c589e40222c3401a8a28
Million1156 <justathrowermc3@gmail.com>
2023-03-01 23:10:19 -0600

    Delete developer_chat.js
    ..

--- developer_chat.js
+++
@@ -1,7 +0,0 @@
-// I don't know what this was used for, so I deleted it.
-// It was causing a bunch of idiotic errors in the console
-// - Aimee
-// yeah someone added this for no reason
-// ----------
-// -- ALBI --
-// ----------
